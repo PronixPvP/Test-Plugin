0c96fbcc12dbe65450e3333034061c1168156ab1
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-09-03 08:55:08 -0700

    Create Main.php

---
+++ src/DRAGKILLS/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DRAGKILLS;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    public \SQLite3 $sqlite;
+
+    public function onEnable()
+    {
+        @mkdir($this->getDataFolder());
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->sqlite = new \SQLite3($this->getDataFolder() . "data.db");
+    }
+
+    public function onJoin(PlayerJoinEvent $event){
+        $this->sqlite->exec("CREATE TABLE IF NOT EXISTS played(id INTEGER PRIMARY KEY, name TEXT, joins INT)");
+        $this->sqlite->exec("INSERT INTO played(name, joins) VALUES('{$event->getPlayer()->getName()}', +1)");
+        $res = $this->sqlite->query('SELECT * FROM played');
+        $event->getPlayer()->sendMessage("Your Name = {$res->fetchArray()["name"]}");
+    }
+}
